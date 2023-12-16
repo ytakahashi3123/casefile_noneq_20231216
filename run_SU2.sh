597246f92a06e854ee3cffb482d9996f20dea28f
ytakahashi3123 <ytakahashi@eng.hokudai.ac.jp>
2023-12-17 02:36:12 +0900

    add file

--- run_SU2.sh
+++ run_SU2.sh
@@ -1,14 +1,4 @@
 #!/bin/bash -x
 
 ncpu=16
-
-DIR_OUTPUT=("output_restart" "output_flow" "output_surface_flow")
-# Make directory
-for (( i = 0; i < ${#DIR_OUTPUT[*]}; i++ ))
-{
-   if [[ ! -e $DIR_OUTPUT[i] ]]; then
-     mkdir ${DIR_OUTPUT[i]}
-   fi
-}
-
 mpirun -np $ncpu SU2_CFD sphere.cfg > log_su2
